a824f47b14200ae98b28daeb92290b35f3e356f6
Ilari Ranin <raninilari@gmail.com>
2026-02-19 09:14:48 +0200

    refactor

--- part1/src/prices.mjs
+++ part1/src/prices.mjs
@@ -21,7 +21,7 @@ function createApp(database) {
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
     const date2 = (req.query.date)
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, date, baseCost, date2);
     res.json({ cost });
   });
 
@@ -31,7 +31,7 @@ function createApp(database) {
     }
   }
 
-  function calculateCost(age, type, date, baseCost) {
+  function calculateCost(age, type, date, baseCost, date2) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
