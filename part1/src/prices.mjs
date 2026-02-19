8f690db522bfebb14a51cf15fd26a76ae940d1e3
Ilari Ranin <raninilari@gmail.com>
2026-02-19 09:20:48 +0200

    refactor

--- part1/src/prices.mjs
+++ part1/src/prices.mjs
@@ -71,7 +71,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && isMonday(date2) && !isHoliday(date)) {
+    if (date && isMonday(date2) && !isHoliday(date, date2)) {
       reduction = 35;
     }
     return reduction;
@@ -81,7 +81,7 @@ function createApp(database) {
     return Temporal.PlainDate.from(date).dayOfWeek === 1;
   }
 
-  function isHoliday(date) {
+  function isHoliday(date, date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
