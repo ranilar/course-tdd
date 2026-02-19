bc8589428da57c362355cf63a58c3f9b7fdadadd
Ilari Ranin <raninilari@gmail.com>
2026-02-19 09:56:27 +0200

    rf

--- part1/src/prices.mjs
+++ part1/src/prices.mjs
@@ -20,7 +20,6 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const date2 = (req.query.date)
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
